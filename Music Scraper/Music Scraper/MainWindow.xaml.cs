7280930340c5ebefaa827ecfbe8f2ad41276c64b
sanecito <sanecito@users.noreply.github.com>
2015-09-15 00:04:33 -0700

    Did some research about mult-filter searching and implemented a simple test to query mp3 and flac files. Next TODOs: Enable users to select dir path, parse files, export to csv, etc

--- Music Scraper/Music Scraper/MainWindow.xaml.cs
+++ Music Scraper/Music Scraper/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿//
+
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +27,25 @@ namespace Music_Scraper
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Scrape_Click(object sender, RoutedEventArgs e)
+        {
+            string samplePath = @"C:\Users\scott\Music";
+            string musicRegex = @".*\.mp3|.*\.flac";
+
+            IEnumerable<string> test = GetFiles(samplePath, musicRegex);
+            foreach (string file in test)
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+        // Taken from http://techmikael.blogspot.com/2010/02/directory-search-with-multiple-filters.html
+        public static IEnumerable<string> GetFiles(string path, string searchPatternExpression = "", SearchOption searchOption = SearchOption.AllDirectories)
+        {
+            Regex reSearchPattern = new Regex(searchPatternExpression);
+            return Directory.EnumerateFiles(path, "*", searchOption).Where(file => reSearchPattern.IsMatch(System.IO.Path.GetFileName(file)));
         }
     }
 }
